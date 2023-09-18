34ee031aab8af067c0eebb88f57c597ef05cdc24
tdascoli <R0hrch@ch>
2023-09-18 22:23:46 +0200

    export #10

---
+++ app/export.php
@@ -0,0 +1,65 @@
+<?php
+
+use CSVDB\Enums\ExportEnum;
+use CSVDBAdmin\CSVDBAdmin;
+
+require '../vendor/autoload.php';
+
+try {
+    if ($_POST) {
+        if (array_key_exists("db_select", $_POST) && array_key_exists("format", $_POST)) {
+            /*
+             * db_select
+             * format
+             * sql query!
+             */
+
+            $admin = new CSVDBAdmin(__DIR__);
+            if (is_array($_POST['db_select'])) {
+                // todo
+                $db = $_POST['db_select'][0];
+            } else {
+                $db = $_POST['db_select'];
+            }
+            $data = $admin->database($db);
+            $records = $data->csvdb()->select()->export($_POST['format']);
+
+            $filename = ".csv";
+            switch ($_POST['format']) {
+                case ExportEnum::JSON:
+                    $filename = ".json";
+                    break;
+                case ExportEnum::SQL:
+                    $filename = ".sql";
+                    break;
+                case ExportEnum::PHP:
+                    $filename = ".php";
+                    break;
+            }
+
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename=export'.$filename);
+            header('Content-Length: ' . strlen($records));
+            header('Connection: close');
+            echo $records;
+            exit;
+        } else {
+            // todo
+//            if (array_key_exists("route", $_POST)) {
+//                header("Location: index.php?route=" . $_POST['route'] . "&error=export");
+//                exit;
+//            }
+            header("Location: index.php?route=/server/export&error=export");
+            exit;
+        }
+    } else {
+        header("Location: index.php?route=/server/export&error=post");
+        exit;
+    }
+} catch (Exception $e) {
+    header("Location: index.php?route=/server/export&error=exception");
+    exit;
+}
+?>
+
--- app/templates/server/export.php
+++ app/templates/server/export.php
@@ -1,3 +1,38 @@
-<h2>
-    Datenbanken Exportieren
-</h2>
+<h2>Datenbanken des aktuell ausgewählten Servers exportieren</h2>
+
+<form method="post" name="export" action="export.php">
+    <div class="exportoptions">
+        <h3>Datenbanken:</h3>
+        <div>
+            <p>
+                <a href="#" onclick="Functions.setSelectOptions('dump', 'db_select[]', true); return false;">Alle
+                    auswählen</a> /
+                <a href="#" onclick="Functions.setSelectOptions('dump', 'db_select[]', false); return false;">Auswahl
+                    entfernen</a>
+            </p>
+
+            <select name="db_select[]" id="db_select" size="5" multiple="">
+                <?php
+                $databases = $admin->databases();
+                foreach ($databases as $database => $csvdb) {
+                    echo '<option value="' . $database . '">' . "\n";
+                    echo $database . "\n";
+                    echo '</option>' . "\n";
+                }
+                ?>
+            </select>
+        </div>
+    </div>
+    <div class="exportoptions">
+        <h3>Format:</h3>
+        <select id="format" name="format">
+            <option selected="selected" value="csv">CSV</option>
+            <option value="json">JSON</option>
+            <option value="php">PHP array</option>
+            <option value="sql">SQL</option>
+        </select>
+    </div>
+    <div>
+        <button class="btn btn-primary ms-1" type="submit" id="button_submit_export">OK</button>
+    </div>
+</form>
