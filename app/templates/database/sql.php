9b129f7c1aa6980fb40670c74463d5489a29b958
Thomas D'Ascoli <thomas@dasco.li>
2024-03-14 10:43:34 +0000

    sql parser and builder

--- app/templates/database/sql.php
+++ app/templates/database/sql.php
@@ -12,32 +12,90 @@ if (!empty($_GET["sql_query"])) {
     $sql_query = $_GET["sql_query"];
 }
 ?>
+<script src="https://cdn.jsdelivr.net/npm/@rechat/squel@5.15.1/dist/squel.min.js"></script>
+<script src="https://unpkg.com/node-sql-parser/umd/mysql.umd.js"></script>
 <script src="script/database/sql.js"></script>
-<div class="card mb-3">
-    <div class="card-header">
-        SQL-Befehl(e) in Datenbank <a
-                href="index.php?route=/database/list&amp;db=<?= $_GET["db"] ?>"><?= $_GET["db"] ?></a> ausführen:
-    </div>
-    <div class="row">
-        <div class="col">
-            <div id="editor" class="sql-editor" style="height: 100%;"><?= $sql_query ?></div>
-            <textarea class="hide" name="sql_query" id="sql_query"><?= $sql_query ?></textarea>
+<script>
+$(document).ready(function () {
+    var query = "<?= $sql_query ?>";
+    
+    const parser = new NodeSQLParser.Parser();
+    const ast = parser.astify(query);
+    console.log(ast);
+    const sql = parser.sqlify(ast);
+    console.log(sql);
+
+    $("#parse_sql_query").val(sql);
+
+    
+    console.log(ast.columns.length);
+    console.log(ast.columns[0].expr.column);
+
+    $("#columns").on('change', function() {
+        console.log($(this).val());
+
+        var test=[];
+        $(this).val().forEach((element) => {
+            // has * only
+            if (ast.columns.length===1 && ast.columns[0].expr.column) {
+                $("#columns").each(function(){
+                    test.push(
+                        { "expr":{
+                                "type":"column_ref",
+                                "table":null,
+                                "column":element
+                            },
+                            "as":null
+                        });
+                });
+            }
+        });      
+
+        console.log(test);
+        ast.columns = test;
+        
+        const sql = parser.sqlify(ast);
+        console.log(sql);
+
+        $("#parse_sql_query").val(sql);
+    });
+
+});
+</script>
+
+<form method="get" name="sqlQuery" id="sqlQuery">
+    <input type="hidden" name="route" value="/database/sql">
+    <input type="hidden" name="db" value="<?= $db ?>">
+
+    <div class="card mb-3">
+        <div class="card-header">
+            SQL-Befehl(e) in Datenbank <a
+                    href="index.php?route=/database/list&amp;db=<?= $_GET["db"] ?>"><?= $_GET["db"] ?></a> ausführen:
         </div>
-        <div class="col-2">
-            <?php
-                $headers = $data->csvdb()->headers();
-                echo '<select size="'.count($headers).'">';
-                foreach($headers as $header) {
-                    echo "<option>".$header."</option>";
-                }
-                echo "</select>";
-            ?>
+        <div class="d-flex">
+            <div class="flex-grow-1">
+                <div id="editor" class="sql-editor" style="height: 100%;"><?= $sql_query ?></div>
+                <textarea class="hide" name="sql_query" id="sql_query"><?= $sql_query ?></textarea>
+            </div>
+            <div class="bg-secondary-subtle" style="width: 200px;">
+                <?php
+                    $headers = $data->csvdb()->headers();
+                    echo '<select class="form-control" id="columns" size="'.count($headers).'" multiple>';
+                    foreach($headers as $header) {
+                        echo "<option>".$header."</option>";
+                    }
+                    echo "</select>";
+                ?>
+            </div>
+        </div>
+        <div>
+            <textarea name="parse_sql_query" id="parse_sql_query" class="form-control"></textarea>
+        </div>
+        <div class="card-footer">
+            <input class="btn btn-primary ms-1" type="submit" id="button_submit_query" name="SQL" tabindex="200" value="OK">
         </div>
     </div>
-    <div class="card-footer">
-        <input class="btn btn-primary ms-1" type="submit" id="button_submit_query" name="SQL" tabindex="200" value="OK">
-    </div>
-</div>
+</form>
 
 <div id="sqlqueryresultsouter">
     <div class="alert alert-danger" role="alert">
