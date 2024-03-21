da20730854a3a55a5f9385dfeaf2e1ae98ee012b
tdascoli <R0hrch@ch>
2024-03-21 21:12:41 +0100

    create

--- app/templates/server/create.php
+++ app/templates/server/create.php
@@ -4,8 +4,14 @@ use CSVDB\Helpers\CSVConfig;
 use CSVDB\Enums\DatatypeEnum;
 
 $config = CSVConfig::default();
-?>
 
+$columns = 4;
+
+if ($_POST) {
+    var_dump($_POST);
+}
+?>
+<!-- add JS for add columns -->
 <form name="config" method="post">
 <div class="card">
     <div class="card-header">
@@ -14,14 +20,14 @@ $config = CSVConfig::default();
                 <label for="database_name" class="col-form-label">Datenbank Name</label>
             </div>
             <div class="col-auto">
-                <input type="text" id="database_name" class="form-control" aria-describedby="database_name">
+                <input type="text" id="database_name" name="database_name" class="form-control" aria-describedby="database_name" required>
             </div>
 
             <div class="col-auto">
                 <label for="add_columns" class="col-form-label">Add</label>
             </div>
             <div class="col-auto">
-                <input type="number" id="add_columns" class="form-control" aria-describedby="add_columns">
+                <input type="number" id="add_columns" class="form-control"  value="1" aria-describedby="add_columns">
             </div>
             <div class="col-auto">
                 column(s)
@@ -47,12 +53,15 @@ $config = CSVConfig::default();
                 </tr>
             </thead>
             <tbody>
+                <?php
+                    for ($i=0;$i<$columns;$i++){
+                ?>
                 <tr class="noclick">
                     <td class="text-center">
-                        <input type="text" name="name" class="form-control"/>
+                        <input type="text" name="row[<?= $i ?>][name]" class="form-control" required/>
                     </td>
                     <td class="text-center">
-                        <select id="encoding" name="type" class="form-select">
+                        <select id="encoding" name="row[<?= $i ?>][type]" class="form-select" required>
                             <option></option>
                             <?php
                                 foreach (DatatypeEnum::getConstants() as $type) {
@@ -66,15 +75,15 @@ $config = CSVConfig::default();
                         </select>                
                     </td>
                     <td class="text-center">
-                        <input type="text" name="default" class="form-control"/>
+                        <input type="text" name="row[<?= $i ?>][default]" class="form-control"/>
                     </td>
                     <td class="text-center">
                         <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="true" name="null_value" id="null_value">
+                            <input class="form-check-input" type="checkbox" value="true" name="row[<?= $i ?>][null_value]" id="null_value">
                         </div>
                     </td>
                     <td class="text-center">
-                        <select id="encoding" name="index" class="form-select">
+                        <select id="encoding" name="row[<?= $i ?>][index]" class="form-select">
                             <option></option>
                             <option value="PRIMARY_KEY">PRIMARY KEY</option>
                             <option value="UNIQUE">UNIQUE</option>
@@ -82,25 +91,28 @@ $config = CSVConfig::default();
                     </td>
                     <td class="text-center">
                         <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="true" name="auto_increment" id="auto_increment">
+                            <input class="form-check-input" type="radio" value="true" name="auto_increment" id="auto_increment">
                         </div>
                     </td>
                     <td class="text-center">
-                        <input type="text" name="comment" class="form-control"/>
+                        <input type="text" name="row[<?= $i ?>][comment]" class="form-control"/>
                     </td>
                 </tr>
+                <?php
+                    }
+                ?>
             </tbody>
         </table>
     </div>
 
     <!-- CONFIG FORM -->
     <div class="w-auto">
-    <fieldset>
-    <legend>Konfiguration</legend>
+    <fieldset class="m-3 w-auto">
+        <legend>Konfiguration</legend>
 
-        <div class="mb-3">
+        <div class="mb-3 w-auto">
             <label for="encoding" class="form-label">Encoding</label>
-            <select id="encoding" name="encoding" class="form-select">
+            <select id="encoding" name="encoding" class="form-select w-auto">
                 <?php
                 foreach (mb_list_encodings() as $encoding) {
                     $selected = "";
@@ -113,12 +125,12 @@ $config = CSVConfig::default();
             </select>            
         </div>
         
-        <div class="mb-3">
+        <div class="mb-3 w-auto">
             <label for="delimiter" class="form-label">Delimiter</label>
-            <input type="text" maxlength="1" name="delimiter" value="<?= $config->delimiter ?>" class="form-control"/>            
+            <input type="text" maxlength="1" name="delimiter" value="<?= $config->delimiter ?>" class="form-control w-auto"/>
         </div>
 
-        <div class="mb-3 form-check">
+        <div class="mb-3  w-auto form-check">
             <?php
                 $cache_true = $config->cache ? 'checked="checked"' : '';
             ?>
@@ -126,7 +138,7 @@ $config = CSVConfig::default();
             <label class="form-check-label" for="cache">Cache</label>
         </div>
         
-        <div class="mb-3 form-check">
+        <div class="mb-3 w-auto form-check">
             <?php
                 $history_true = $config->history ? 'checked="checked"' : '';
             ?>
